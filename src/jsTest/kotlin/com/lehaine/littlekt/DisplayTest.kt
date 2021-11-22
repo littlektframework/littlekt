1471232ef7af4eb88082a0ee772672dd0f05fd39
LeHaine <colt.daily@gmail.com>
2021-11-22 14:50:35 -0500

    update js webpack major version to 4
    add back internal utils to JS
    
    update JS DisplayTst

--- src/jsTest/kotlin/com/lehaine/littlekt/DisplayTest.kt
+++ src/jsTest/kotlin/com/lehaine/littlekt/DisplayTest.kt
@@ -1,12 +1,16 @@
 package com.lehaine.littlekt
 
+import kotlinx.browser.window
+
 /**
  * @author Colton Daily
  * @date 11/22/2021
  */
 fun main() {
+    val rootPath =
+        (window.location.protocol + "//" + window.location.host + window.location.pathname).replace("index.html", "")
     LittleKtAppBuilder(
-        configBuilder = { ApplicationConfiguration("JS - Display Test") },
+        configBuilder = { ApplicationConfiguration("JS - Display Test", rootPath = rootPath) },
         gameBuilder = { DisplayTest(it) })
         .start()
 }
