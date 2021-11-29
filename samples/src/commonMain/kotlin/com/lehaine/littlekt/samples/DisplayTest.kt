de1b94504f3daaef0a4c6b7148f6f422ae48eeaa
LeHaine <colt.daily@gmail.com>
2021-11-29 18:15:49 -0500

    clean up code

--- samples/src/commonMain/kotlin/com/lehaine/littlekt/samples/DisplayTest.kt
+++ samples/src/commonMain/kotlin/com/lehaine/littlekt/samples/DisplayTest.kt
@@ -4,10 +4,7 @@ import com.lehaine.littlekt.Application
 import com.lehaine.littlekt.LittleKt
 import com.lehaine.littlekt.createShader
 import com.lehaine.littlekt.graphics.*
-import com.lehaine.littlekt.graphics.shader.TestModel
-import com.lehaine.littlekt.graphics.shader.fragment.GlyphFragmentShader
 import com.lehaine.littlekt.graphics.shader.fragment.SimpleColorFragmentShader
-import com.lehaine.littlekt.graphics.shader.vertex.GlyphVertexShader
 import com.lehaine.littlekt.graphics.shader.vertex.SimpleColorVertexShader
 import com.lehaine.littlekt.input.InputProcessor
 import com.lehaine.littlekt.input.Key
