12cc4e333dfdd723e67c1c96dcc1ab6293a866eb
LeHaine <colt.daily@gmail.com>
2021-12-13 18:17:18 -0500

    glsl: add more func param types and fix issues with funcs return delegates

--- core/src/commonMain/kotlin/com/lehaine/littlekt/graphics/shader/generator/type/bool.kt
+++ core/src/commonMain/kotlin/com/lehaine/littlekt/graphics/shader/generator/type/bool.kt
@@ -4,7 +4,7 @@ package com.lehaine.littlekt.graphics.shader.generator.type
  * @author Colton Daily
  * @date 11/25/2021
  */
-data class BoolResult(val value: String) {
+data class BoolResult(var value: String) {
     infix fun or(a: BoolResult): BoolResult = BoolResult("(${this.value} || ${a.value})")
     infix fun and(a: BoolResult): BoolResult = BoolResult("(${this.value} && ${a.value})")
 }
