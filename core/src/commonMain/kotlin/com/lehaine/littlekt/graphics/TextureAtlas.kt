b1605692ad660d903a8db75aa4b17f7f621919a3
LeHaine <colt.daily@gmail.com>
2021-12-29 13:25:25 -0500

    texture atlas: update getByPrefix to check if name starts with passed in prefix vs a fully matching

--- core/src/commonMain/kotlin/com/lehaine/littlekt/graphics/TextureAtlas.kt
+++ core/src/commonMain/kotlin/com/lehaine/littlekt/graphics/TextureAtlas.kt
@@ -27,7 +27,7 @@ class TextureAtlas internal constructor(private val textures: Map<String, Textur
      * @param prefix the name prefix of the [TextureSlice]
      * @returns the first [Entry] that matches the supplied prefix
      */
-    fun getByPrefix(prefix: String): Entry = entries.first { it.name == prefix }
+    fun getByPrefix(prefix: String): Entry = entries.first { it.name.startsWith(prefix) }
 
     operator fun get(name: String): Entry = entriesMap[name] ?: error("Can't find $name in atlas.")
 
