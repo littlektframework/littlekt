580cc5d63ab13980d674ebd703373f4236603334
LeHaine <colt.daily@gmail.com>
2021-12-30 14:10:09 -0500

    input: add gamepad polling support to JS (#11)

--- core/src/jsMain/kotlin/com/lehaine/littlekt/input/JsInput.kt
+++ core/src/jsMain/kotlin/com/lehaine/littlekt/input/JsInput.kt
@@ -1,6 +1,9 @@
 package com.lehaine.littlekt.input
 
+import com.lehaine.littlekt.math.geom.Point
+import com.lehaine.littlekt.util.fastForEach
 import kotlinx.browser.document
+import kotlinx.browser.window
 import org.w3c.dom.HTMLCanvasElement
 import org.w3c.dom.TouchEvent
 import org.w3c.dom.events.Event
@@ -20,9 +23,14 @@ class JsInput(val canvas: HTMLCanvasElement) : Input {
     private var logicalMouseY = 0f
     private var _deltaX = 0f
     private var _deltaY = 0f
-    private var lastChar: Char = 0.toChar()
 
     override var inputProcessor: InputProcessor? = null
+
+    override val gamepads: Array<GamepadInfo> = Array(8) { GamepadInfo(it) }
+
+    private val _connectedGamepads = mutableListOf<GamepadInfo>()
+    override val connectedGamepads: List<GamepadInfo>
+        get() = _connectedGamepads
 
     init {
         document.addEventListener("keydown", ::keyDown, false)
@@ -35,6 +43,18 @@ class JsInput(val canvas: HTMLCanvasElement) : Input {
         canvas.addEventListener("mouseup", ::mouseUp, false)
         canvas.addEventListener("mousemove", ::mouseMove, false)
         canvas.addEventListener("wheel", ::scroll, false)
+
+        window.addEventListener("gamepadconnected", { e ->
+            val ge = e.unsafeCast<JsGamepadEvent>()
+            gamepads[ge.gamepad.index].connected = true
+            _connectedGamepads += gamepads[ge.gamepad.index]
+        })
+        window.addEventListener("gamepaddisconnected", { e ->
+            val ge = e.unsafeCast<JsGamepadEvent>()
+            gamepads[ge.gamepad.index].connected = false
+            _connectedGamepads -= gamepads[ge.gamepad.index]
+        })
+        checkForGamepads()
     }
 
     private fun keyDown(event: Event) {
@@ -124,6 +144,7 @@ class JsInput(val canvas: HTMLCanvasElement) : Input {
     }
 
     fun update() {
+        updateGamepads()
         inputManager.processEvents(inputProcessor)
     }
 
@@ -131,6 +152,53 @@ class JsInput(val canvas: HTMLCanvasElement) : Input {
         inputManager.reset()
         _deltaX = 0f
         _deltaY = 0f
+    }
+
+    fun checkForGamepads() {
+        try {
+            if (navigator.getGamepads != null) {
+                val jsGamepads = navigator.getGamepads().unsafeCast<JsArray<JsGamePad?>>()
+                gamepads.fastForEach { it.connected = false }
+
+                for (gamepadId in 0 until jsGamepads.length) {
+                    val controller = jsGamepads[gamepadId] ?: continue
+                    val gamepad = gamepads.getOrNull(gamepadId) ?: continue
+                    gamepad.apply {
+                        this.connected = controller.connected
+                        this.name = controller.id
+                    }
+                }
+            }
+        } catch (e: dynamic) {
+            console.error(e)
+        }
+    }
+
+    private fun updateGamepads() {
+        if (connectedGamepads.isEmpty()) return
+
+        try {
+            if (navigator.getGamepads != null) {
+                val jsGamepads = navigator.getGamepads().unsafeCast<JsArray<JsGamePad?>>()
+                gamepads.fastForEach { it.connected = false }
+
+                for (gamepadId in 0 until jsGamepads.length) {
+                    val controller = jsGamepads[gamepadId] ?: continue
+                    val gamepad = gamepads.getOrNull(gamepadId) ?: continue
+                    gamepad.apply {
+                        for (n in 0 until controller.buttons.length) {
+                            val button = controller.buttons[n]
+                            this.rawButtonsPressed[n] = button.value
+                        }
+                        for (n in 0 until controller.axes.length) {
+                            this.rawAxes[n] = controller.axes[n]
+                        }
+                    }
+                }
+            }
+        } catch (e: dynamic) {
+            console.error(e)
+        }
     }
 
     override val x: Int
@@ -147,6 +215,15 @@ class JsInput(val canvas: HTMLCanvasElement) : Input {
         get() = inputManager.justTouched
     override val pressure: Float
         get() = getPressure(Pointer.POINTER1)
+
+    override val axisLeftX: Float
+        get() = getGamepadJoystickXDistance(GameStick.LEFT)
+    override val axisLeftY: Float
+        get() = getGamepadJoystickYDistance(GameStick.LEFT)
+    override val axisRightX: Float
+        get() = getGamepadJoystickXDistance(GameStick.RIGHT)
+    override val axisRightY: Float
+        get() = getGamepadJoystickYDistance(GameStick.RIGHT)
 
     override fun getX(pointer: Pointer): Int {
         return if (pointer == Pointer.POINTER1) x else 0
@@ -184,7 +261,63 @@ class JsInput(val canvas: HTMLCanvasElement) : Input {
         return inputManager.isKeyJustReleased(key)
     }
 
+    override fun isGamepadButtonJustPressed(button: GameButton, gamepad: Int): Boolean {
+        return inputManager.isGamepadButtonJustPressed(button, gamepad)
+    }
+
+    override fun isGamepadButtonPressed(button: GameButton, gamepad: Int): Boolean {
+        return inputManager.isGamepadButtonPressed(button, gamepad)
+    }
+
+    override fun isGamepadButtonJustReleased(button: GameButton, gamepad: Int): Boolean {
+        return inputManager.isGamepadButtonJustReleased(button, gamepad)
+    }
+
+    override fun getGamepadButtonPressure(button: GameButton, gamepad: Int): Float {
+        return if (connectedGamepads.isNotEmpty()) gamepads[gamepad][button] else 0f
+    }
+
+    override fun getGamepadJoystickDistance(stick: GameStick, gamepad: Int): Point {
+        return if (connectedGamepads.isNotEmpty()) gamepads[gamepad][stick] else Point.ZERO
+    }
+
+    override fun getGamepadJoystickXDistance(stick: GameStick, gamepad: Int): Float {
+        return if (connectedGamepads.isNotEmpty()) gamepads[gamepad].getX(stick) else 0f
+    }
+
+    override fun getGamepadJoystickYDistance(stick: GameStick, gamepad: Int): Float {
+        return if (connectedGamepads.isNotEmpty()) gamepads[gamepad].getY(stick) else 0f
+    }
+
     override fun setCursorPosition(x: Int, y: Int) {
         TODO("Not yet implemented")
     }
+}
+
+private external val navigator: dynamic
+
+private external interface JsArray<T> {
+    val length: Int
+}
+
+private inline operator fun <T> JsArray<T>.get(index: Int): T = this.asDynamic()[index]
+
+private external interface JsGamepadButton {
+    val value: Float
+    val pressed: Boolean
+}
+
+private external interface JsGamePad {
+    val axes: JsArray<Float>
+    val buttons: JsArray<JsGamepadButton>
+    val connected: Boolean
+    val id: String
+    val index: Int
+    val mapping: String
+    val timestamp: Float
+}
+
+@JsName("GamepadEvent")
+private external interface JsGamepadEvent {
+    val gamepad: JsGamePad
 }
