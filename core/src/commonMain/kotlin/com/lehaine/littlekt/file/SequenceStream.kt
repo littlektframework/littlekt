cd8ad5e341ea7d3404e240088dc4059e0c85f11a
LeHaine <colt.daily@gmail.com>
2022-01-12 16:52:38 -0500

    sequence stream: add readChunk and reset methods to the interface and JVM implementations

--- core/src/commonMain/kotlin/com/lehaine/littlekt/file/SequenceStream.kt
+++ core/src/commonMain/kotlin/com/lehaine/littlekt/file/SequenceStream.kt
@@ -1,10 +1,25 @@
 package com.lehaine.littlekt.file
 
 /**
+ * A buffer sequence stream to read a sequence of bytes.
  * @author Colton Daily
  * @date 1/12/2022
  */
-interface SequenceStream: Sequence<Byte> {
+interface SequenceStream : Sequence<Byte> {
 
+    /**
+     * Reads the next chunk of the specified [size] or until it reaches the end of the [SequenceStream].
+     * @return a new list with the read values.
+     */
+    fun readChunk(size: Int): List<Byte>
+
+    /**
+     * Resets the chunk iterator back to the beginning.
+     */
+    fun reset()
+
+    /**
+     * Closes the stream and prevents it from being read again.
+     */
     fun close()
 }
--- core/src/jvmMain/kotlin/com/lehaine/littlekt/file/JvmSequenceStream.kt
+++ core/src/jvmMain/kotlin/com/lehaine/littlekt/file/JvmSequenceStream.kt
@@ -7,12 +7,29 @@ import java.io.InputStream
  * @date 1/12/2022
  */
 class JvmSequenceStream(private val stream: InputStream) : SequenceStream {
+    private val bufferedStream = stream.buffered()
+    private var chunkIterator = bufferedStream.iterator()
 
     override fun iterator(): Iterator<Byte> {
-        return stream.buffered().iterator()
+        return bufferedStream.iterator()
+    }
+
+    override fun readChunk(size: Int): List<Byte> {
+        val list = mutableListOf<Byte>()
+        var i = 0
+        while (i < size && chunkIterator.hasNext()) {
+            list += chunkIterator.nextByte()
+            i++
+        }
+        return list
+    }
+
+    override fun reset() {
+        chunkIterator = bufferedStream.iterator()
     }
 
     override fun close() {
         stream.close()
+        bufferedStream.close()
     }
 }
