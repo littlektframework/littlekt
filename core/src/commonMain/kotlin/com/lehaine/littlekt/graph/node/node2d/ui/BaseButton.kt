3f075cc5bea885600ecace344709c6f9d8ca348c
LeHaine <colt.daily@gmail.com>
2022-01-17 14:16:55 -0500

    button: finish impl of BaseButton control

--- core/src/commonMain/kotlin/com/lehaine/littlekt/graph/node/node2d/ui/BaseButton.kt
+++ core/src/commonMain/kotlin/com/lehaine/littlekt/graph/node/node2d/ui/BaseButton.kt
@@ -3,24 +3,109 @@ package com.lehaine.littlekt.graph.node.node2d.ui
 import com.lehaine.littlekt.graph.node.component.InputEvent
 import com.lehaine.littlekt.util.Signal
 import com.lehaine.littlekt.util.TypedSignal
+import kotlin.js.JsName
 
 /**
+ * The abstract base for different buttons. Other types of buttons inherit from this.
  * @author Colton Daily
  * @date 1/17/2022
  */
 abstract class BaseButton : Control() {
 
     private val status = Status()
-    private var toggleMode = false
-    private val actionMode = ActionMode.BUTTON_RELEASE
+    private var _toggleMode = false
+    private val buttonGroup: ButtonGroup = ButtonGroup()
+
+    val drawMode: DrawMode
+        get() {
+            if (status.disabled) {
+                return DrawMode.DISABLED
+            }
+            if (!status.pressAttempt && status.hovering) {
+                if (status.pressed) {
+                    return DrawMode.HOVER_PRESSED
+                }
+                return DrawMode.HOVER
+            } else {
+                var pressing: Boolean
+                if (status.pressAttempt) {
+                    pressing = status.pressingInside
+                    if (status.pressed) {
+                        pressing = !pressing
+                    }
+                } else {
+                    pressing = status.pressed
+                }
+                return if (pressing) {
+                    DrawMode.PRESSRED
+                } else {
+                    DrawMode.NORMAL
+                }
+            }
+        }
+    var toggleMode: Boolean
+        get() = _toggleMode
+        set(value) {
+            if (!value) {
+                pressed = false
+            }
+            _toggleMode = value
+        }
+    var actionMode = ActionMode.BUTTON_RELEASE
+    val pressing: Boolean get() = status.pressAttempt
+
+    @JsName("isPressed")
+    var pressed: Boolean = false
+        get() = if (_toggleMode) status.pressed else status.pressAttempt
+        set(value) {
+            if (!_toggleMode) return
+            if (field == value) return
+            status.pressed = value
+
+            if (value) {
+                unpressGroup()
+            }
+            _toggled(status.pressed)
+        }
+    val hovered: Boolean get() = status.hovering
+    var disabled: Boolean = false
+        get() = status.disabled
+        set(value) {
+            if (field == value) return
+            status.disabled = value
+            if (value) {
+                if (!_toggleMode) {
+                    status.pressed = false
+                }
+                status.pressAttempt = false
+                status.pressingInside = false
+            }
+        }
 
     val onPressed: Signal = Signal()
     val onToggled: TypedSignal<Boolean> = TypedSignal()
     val onButtonDown: Signal = Signal()
     val onButtonUp: Signal = Signal()
 
+    override fun _onRemovedFromScene() {
+        super._onRemovedFromScene()
+        if (!toggleMode) {
+            status.pressed = false
+        }
+        status.hovering = false
+        status.pressAttempt = false
+        status.pressingInside = false
+    }
+
     override fun uiInput(event: InputEvent) {
         super.uiInput(event)
+        if (event.type == InputEvent.Type.MOUSE_ENTER) {
+            status.hovering = true
+        }
+        if (event.type == InputEvent.Type.MOUSE_EXIT) {
+            status.hovering = false
+        }
+
         if (event.type == InputEvent.Type.TOUCH_DOWN) {
             status.pressAttempt = true
             status.pressingInside = true
@@ -28,7 +113,7 @@ abstract class BaseButton : Control() {
         }
 
         if (status.pressAttempt && status.pressingInside) {
-            if (toggleMode) {
+            if (_toggleMode) {
                 val isPressed = event.type == InputEvent.Type.TOUCH_DOWN
                 // TODO check if shortcut input then: isPressed = false
                 if ((isPressed && actionMode == ActionMode.BUTTON_PRESS) || (!isPressed && actionMode == ActionMode.BUTTON_RELEASE)) {
@@ -37,7 +122,7 @@ abstract class BaseButton : Control() {
                         status.pressingInside = false
                     }
                     status.pressed = !status.pressed
-                    // TODO _unpressGroup
+                    unpressGroup()
                     _toggled(status.pressed)
                     _pressed()
                 }
@@ -58,6 +143,19 @@ abstract class BaseButton : Control() {
         }
     }
 
+    private fun unpressGroup() {
+        if (toggleMode) {
+            status.pressed = true
+        }
+
+        buttonGroup.buttons.forEach {
+            if (it == this) {
+                return@forEach
+            }
+            it.pressed = false
+        }
+    }
+
     private fun _pressed() {
         pressed()
         onPressed.emit()
@@ -68,9 +166,17 @@ abstract class BaseButton : Control() {
         onToggled.emit(pressed)
     }
 
-    open fun pressed() = Unit
+    protected open fun pressed() = Unit
 
-    open fun toggled(pressed: Boolean) = Unit
+    protected open fun toggled(pressed: Boolean) = Unit
+
+    enum class DrawMode {
+        NORMAL,
+        PRESSRED,
+        HOVER,
+        DISABLED,
+        HOVER_PRESSED
+    }
 
     enum class ActionMode {
         BUTTON_PRESS,
@@ -84,4 +190,9 @@ abstract class BaseButton : Control() {
         var pressingInside = false
         var disabled = false
     }
+}
+
+class ButtonGroup {
+    val buttons = mutableSetOf<BaseButton>()
+
 }
