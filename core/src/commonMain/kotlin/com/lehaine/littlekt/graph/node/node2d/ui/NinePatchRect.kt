743649923de2473370588c556e023bab65d7e438
LeHaine <colt.daily@gmail.com>
2022-02-23 16:51:46 -0500

    nine patch rect: refactor to set texture slice and set nine patch margins directly vs having to create own ninepatch object

--- core/src/commonMain/kotlin/com/lehaine/littlekt/graph/node/node2d/ui/NinePatchRect.kt
+++ core/src/commonMain/kotlin/com/lehaine/littlekt/graph/node/node2d/ui/NinePatchRect.kt
@@ -25,12 +25,38 @@ inline fun SceneGraph<*>.ninePatchRect(callback: @SceneGraphDslMarker NinePatchR
  */
 open class NinePatchRect : Control() {
 
-    var ninePatch: NinePatch = NinePatch(Textures.white, 0, 0, 0, 0)
+    /**
+     * The width of the 9-patch's left column.
+     */
+    var left: Int = 0
+
+    /**
+     * The width of the 9-patch's right column.
+     */
+    var right: Int = 0
+
+    /**
+     * The height of the 9-patch's top row.
+     */
+    var top: Int = 0
+
+    /**
+     * The height of the 9-patch's bottom row.
+     */
+    var bottom: Int = 0
+
+    /**
+     * The texture to be used as a [NinePatch].
+     */
+    var texture: TextureSlice = Textures.white
         set(value) {
             if (field == value) return
             field = value
+            ninePatch = NinePatch(value, left, right, bottom, top)
             onMinimumSizeChanged()
         }
+
+    private var ninePatch: NinePatch = NinePatch(Textures.white, left, right, bottom, top)
 
     override fun calculateMinSize() {
         if (!minSizeInvalid) return
