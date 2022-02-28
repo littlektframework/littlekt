1b61e3b082ab7129283bfdf42854392b83cf9440
LeHaine <colt.daily@gmail.com>
2022-02-28 16:37:41 -0500

    tiled: add base TiledJson

---
+++ core/src/commonMain/kotlin/com/lehaine/littlekt/file/tiled/TiledJson.kt
@@ -0,0 +1,128 @@
+package com.lehaine.littlekt.file.tiled
+
+import kotlinx.serialization.Serializable
+
+/**
+ * @author Colton Daily
+ * @date 2/28/2022
+ */
+@Serializable
+internal data class TiledMapData(
+    val backgroundColor: String,
+    val layers: List<TiledLayerData> = emptyList(),
+    val width: Int,
+    val height: Int,
+    val properties: List<TiledProperty> = emptyList(),
+    val tilewidth: Int,
+    val tileheight: Int,
+    val tilesets: List<TiledTilesetData> = emptyList()
+)
+
+@Serializable
+internal data class TiledProperty(val key: String, val type: String, val value: String)
+
+@Serializable
+internal data class TiledLayerData(
+    val id: Int,
+    val draworder: String,
+    val data: List<Int> = emptyList(),
+    val objects: List<TiledObjectData> = emptyList(),
+    val width: Int,
+    val height: Int,
+    val x: Int,
+    val y: Int,
+    val properties: List<TiledProperty> = emptyList(),
+    val name: String,
+    val visible: Boolean,
+    val opacity: Float
+)
+
+@Serializable
+internal data class TiledObjectData(
+    val id: Int,
+    val gid: Int,
+    val name: String,
+    val properties: List<TiledProperty> = emptyList(),
+    val x: Float,
+    val y: Float,
+    val width: Float,
+    val height: Float,
+    val rotation: Float,
+    val visible: Boolean,
+    val point: Boolean,
+    val ellipse: Boolean,
+    val text: TiledTextData? = null
+)
+
+@Serializable
+internal data class TiledTextData(
+    val text: String,
+    val wrap: Boolean,
+    val bold: Boolean,
+    val color: String,
+    val halign: String,
+    val valign: String,
+    val pixelsize: Int,
+    val fontfamily: String
+)
+
+@Serializable
+internal data class TiledTilesetData(
+    val firstgid: Int,
+    val rows: Int,
+    val columns: Int,
+    val source: String,
+    val tilewidth: Int,
+    val tileheight: Int,
+    val wangsets: List<TiledWangSetData> = emptyList(),
+    val imagewidth: Int,
+    val imageheight: Int,
+    val margin: Int,
+    val spacing: Int,
+    val grid: TiledGridData? = null,
+    val objectalignment: String,
+    val terrains: List<TiledTerrainData> = emptyList(),
+    val tiles: List<TiledTileData> = emptyList()
+)
+
+@Serializable
+internal data class TiledGridData(val width: Int, val height: Int, val orientation: String)
+
+@Serializable
+internal data class TiledTerrainData(val name: String, val properties: List<TiledProperty> = emptyList(), val tile: Int)
+
+@Serializable
+internal data class TiledTileData(
+    val animation: List<TiledTileFrame> = emptyList(),
+    val id: Int,
+    val imageheight: Int,
+    val imagewidth: Int,
+    val objectgroup: TiledLayerData? = null,
+    val probability: Float,
+    val properties: List<TiledProperty> = emptyList(),
+    val terrain: List<Int>,
+    val type: String
+)
+
+@Serializable
+internal data class TiledTileFrame(val duration: Int, val tiledid: Int)
+
+@Serializable
+internal data class TiledWangSetData(
+    val colors: List<TiledWangColorData> = emptyList(),
+    val name: String,
+    val properties: List<TiledProperty> = emptyList(),
+    val type: String,
+    val wangtiles: List<TiledWangTileData> = emptyList()
+)
+
+@Serializable
+internal data class TiledWangColorData(
+    val color: String,
+    val name: String,
+    val tile: Int,
+    val properties: List<TiledProperty> = emptyList()
+)
+
+@Serializable
+internal data class TiledWangTileData(val tileid: Int, val wangid: List<Int>)
