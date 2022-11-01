2dcb98a47365e6f46093b447da7835cae369e038
LeHaine <colt.daily@gmail.com>
2022-11-01 14:22:38 -0400

    graph: update EmptyDrawable to be an immutable singleton object which will throw an exception if its values attempt to change
    misc: add new 'emptyDrawable()' util function

--- core/src/commonMain/kotlin/com/lehaine/littlekt/graph/node/component/EmptyDrawable.kt
+++ core/src/commonMain/kotlin/com/lehaine/littlekt/graph/node/component/EmptyDrawable.kt
@@ -6,17 +6,47 @@ import com.lehaine.littlekt.math.geom.Angle
 
 /**
  * A [Drawable] with no size or margin and draws nothing.
+ *
+ * **WARNING**: This drawable is immutable and attempting to change a value will throw an exception!
  * @author Colton Daily
  * @date 10/19/2022
  */
-class EmptyDrawable : Drawable {
-    override var marginLeft: Float = 0f
-    override var marginRight: Float = 0f
-    override var marginTop: Float = 0f
-    override var marginBottom: Float = 0f
-    override var minWidth: Float = 0f
-    override var minHeight: Float = 0f
-    override var modulate: Color = Color.WHITE
+object EmptyDrawable : Drawable {
+    override var marginLeft: Float
+        get() = 0f
+        set(_) {
+            error("You must not change the margins or minimum size of the EmptyDrawable!")
+        }
+    override var marginRight: Float
+        get() = 0f
+        set(_) {
+            error("You must not change the margins or minimum size of the EmptyDrawable!")
+        }
+    override var marginTop: Float
+        get() = 0f
+        set(_) {
+            error("You must not change the margins or minimum size of the EmptyDrawable!")
+        }
+    override var marginBottom: Float
+        get() = 0f
+        set(_) {
+            error("You must not change the margins or minimum size of the EmptyDrawable!")
+        }
+    override var minWidth: Float
+        get() = 0f
+        set(_) {
+            error("You must not change the margins or minimum size of the EmptyDrawable!")
+        }
+    override var minHeight: Float
+        get() = 0f
+        set(_) {
+            error("You must not change the margins or minimum size of the EmptyDrawable!")
+        }
+    override var modulate: Color
+        get() = Color.WHITE
+        set(_) {
+            error("You must not change the color  of the EmptyDrawable!")
+        }
 
     override fun draw(
         batch: Batch,
@@ -32,3 +62,10 @@ class EmptyDrawable : Drawable {
         // Do nothing. nothing to draw
     }
 }
+
+/**
+ * Returns the [EmptyDrawable] singleton object.
+ *
+ * **WARNING**: This drawable is immutable and attempting to change a value will throw an exception!
+ */
+fun emptyDrawable(): EmptyDrawable = EmptyDrawable
