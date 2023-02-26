1344242691b85379e3319dbccf874c30773e86e6
LeHaine <colt.daily@gmail.com>
2023-02-25 19:07:24 -0500

    glsl: remove unused definitons from function instructions

--- core/src/commonMain/kotlin/com/lehaine/littlekt/graphics/shader/generator/GlslGenerator.kt
+++ core/src/commonMain/kotlin/com/lehaine/littlekt/graphics/shader/generator/GlslGenerator.kt
@@ -302,6 +302,7 @@ abstract class GlslGenerator : GlslProvider {
 
     private fun removeUnusedDefinitions() {
         instructions.removeAll { it.result.contains("{def}") }
+        functionInstructions.removeAll { it.result.contains("{def}") }
     }
 
     fun <T : Variable> uniform(factory: (GlslGenerator) -> T, precision: Precision = Precision.DEFAULT) =
@@ -682,8 +683,12 @@ abstract class GlslGenerator : GlslProvider {
     fun inverse(v: Mat3) = Mat3(this, "inverse(${v.value})")
     fun inverse(v: Mat4) = Mat4(this, "inverse(${v.value})")
 
-    fun shadow2D(sampler: ShadowTexture2D, v: Vec2) = Vec4(this, "shadow2D(${sampler.value}, ${v.value})")
-    fun texture2D(sampler: Sampler2D, v: Vec2) = Vec4(this, "texture2D(${sampler.value}, ${v.value})")
+    fun shadow2D(sampler: ShadowTexture2D, v: Vec2) =
+        ConstructorDelegate(Vec4(this, "shadow2D(${sampler.value}, ${v.value})"))
+
+    fun texture2D(sampler: Sampler2D, v: Vec2) =
+        ConstructorDelegate(Vec4(this, "texture2D(${sampler.value}, ${v.value})"))
+
     fun texture(sampler: Sampler2DArray, v: Vec3) = Vec4(this, "texture(${sampler.value}, ${v.value})")
 
     fun float(genValue: (() -> GLFloat)? = null) = ConstructorDelegate(GLFloat(this), null, genValue)
