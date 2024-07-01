707f8cd56276ed5e22fa551139be4a8a30bec71a
LeHaine <colt.daily@gmail.com>
2024-07-01 08:07:14 -0400

    core: update internal Context 'ownedContext' in JS & JVM to throw an error if referenced

--- core/src/jvmMain/kotlin/com/littlekt/ContextExt.jvm.kt
+++ core/src/jvmMain/kotlin/com/littlekt/ContextExt.jvm.kt
@@ -9,40 +9,40 @@ import com.littlekt.util.Clipboard
 internal actual var ownedContext: Context =
     object : Context() {
         override val stats: AppStats
-            get() = TODO("Not yet implemented")
+            get() = error("Internal Context that shouldn't ever be referenced!")
 
         override val configuration: ContextConfiguration
-            get() = TODO("Not yet implemented")
+            get() = error("Internal Context that shouldn't ever be referenced!")
 
         override val graphics: Graphics
-            get() = TODO("Not yet implemented")
+            get() = error("Internal Context that shouldn't ever be referenced!")
 
         override val input: Input
-            get() = TODO("Not yet implemented")
+            get() = error("Internal Context that shouldn't ever be referenced!")
 
         override val logger: Logger
-            get() = TODO("Not yet implemented")
+            get() = error("Internal Context that shouldn't ever be referenced!")
 
         override val vfs: Vfs
-            get() = TODO("Not yet implemented")
+            get() = error("Internal Context that shouldn't ever be referenced!")
 
         override val resourcesVfs: VfsFile
-            get() = TODO("Not yet implemented")
+            get() = error("Internal Context that shouldn't ever be referenced!")
 
         override val storageVfs: VfsFile
-            get() = TODO("Not yet implemented")
+            get() = error("Internal Context that shouldn't ever be referenced!")
 
         override val platform: Platform
-            get() = TODO("Not yet implemented")
+            get() = error("Internal Context that shouldn't ever be referenced!")
 
         override val clipboard: Clipboard
-            get() = TODO("Not yet implemented")
+            get() = error("Internal Context that shouldn't ever be referenced!")
 
         override fun start(build: (app: Context) -> ContextListener) {
-            TODO("Not yet implemented")
+            error("Internal Context that shouldn't ever be referenced!")
         }
 
         override fun close() {
-            TODO("Not yet implemented")
+            error("Internal Context that shouldn't ever be referenced!")
         }
     }
