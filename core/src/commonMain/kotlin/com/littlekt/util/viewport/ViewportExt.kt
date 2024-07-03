f6f5ae389725cf98342bc5d447f66663f0c13832
LeHaine <colt.daily@gmail.com>
2024-07-03 07:56:43 -0400

    scene-graph: update SceneGraph to only set sceneCanvas Node spriteShader to the Batch.defaultShader, if it owns the batch, otherwise it will create a new Shader

--- core/src/commonMain/kotlin/com/littlekt/util/viewport/ViewportExt.kt
+++ core/src/commonMain/kotlin/com/littlekt/util/viewport/ViewportExt.kt
@@ -2,6 +2,12 @@ package com.littlekt.util.viewport
 
 import com.littlekt.graphics.webgpu.RenderPassEncoder
 
+/**
+ * Sets the viewport used during the rasterization stage to linear map from normalized device
+ * coordinates to viewport coordinates.
+ *
+ * @param viewport viewport data use on the render pass
+ */
 fun RenderPassEncoder.setViewport(viewport: Viewport) {
     setViewport(viewport.x, viewport.y, viewport.width, viewport.height)
 }
