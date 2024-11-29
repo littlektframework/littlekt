2f82842ee89422723794b13a14c4477f9e508fbf
LeHaine <colt.daily@gmail.com>
2024-11-29 12:01:14 -0500

    example: tweak gltf example to prepare for texture

--- examples/src/commonMain/kotlin/com/littlekt/examples/SimpleGltfExample.kt
+++ examples/src/commonMain/kotlin/com/littlekt/examples/SimpleGltfExample.kt
@@ -24,14 +24,23 @@ class SimpleGltfExample(context: Context) : ContextListener(context) {
         """
         alias float4 = vec4<f32>;
         alias float3 = vec3<f32>;
+        alias float2 = vec2<f32>;
 
         struct VertexInput {
             @location(0) position: float3,
+            @location(1) texcoords: float2,
         };
-
+        
         struct VertexOutput {
             @builtin(position) position: float4,
             @location(0) world_pos: float3,
+            @location(1) texcoords: float2,
+        };
+        
+        struct MaterialParams {
+            base_color_factor: float4,
+            metallic_factor: f32,
+            roughness_factor: f32,
         };
 
         struct ViewParams {
@@ -44,15 +53,32 @@ class SimpleGltfExample(context: Context) : ContextListener(context) {
 
         @group(0) @binding(0)
         var<uniform> view_params: ViewParams;
+        
         @group(1) @binding(0)
         var<uniform> node_params: NodeParams;
+        
+        @group(2) @binding(0)
+        var<uniform> material_params: MaterialParams;
+        
+        @group(2) @binding(1)
+        var base_color_sampler: sampler;
+        
+        @group(2) @binding(2)
+        var base_color_texture: texture_2d<f32>;
+        
+        fn linear_to_srgb(x: f32) -> f32 {
+            if (x <= 0.0031308) {
+                return 12.92 * x;
+            }
+            return 1.055 * pow(x, 1.0 / 2.4) - 0.055;
+        }
 
         @vertex
         fn vs_main(vert: VertexInput) -> VertexOutput {
             var out: VertexOutput;
             out.position = view_params.view_proj * node_params.transform * float4(vert.position, 1.0);
             out.world_pos = vert.position.xyz;
-            return out;
+            out.texcoords = vert.texcoords;
         };
 
         @fragment
@@ -60,7 +86,14 @@ class SimpleGltfExample(context: Context) : ContextListener(context) {
             let dx = dpdx(in.world_pos);
             let dy = dpdy(in.world_pos);
             let n = normalize(cross(dx, dy));
-            return float4((n + 1.0) * 0.5, 1.0);
+            let base_color = textureSample(base_color_texture, base_color_sampler, in.texcoords);
+            var color = material_params.base_color_factor * base_color;
+        
+            color.x = linear_to_srgb(color.x);
+            color.y = linear_to_srgb(color.y);
+            color.z = linear_to_srgb(color.z);
+            color.w = 1.0;
+            return color;
         }
     """
             .trimIndent()
