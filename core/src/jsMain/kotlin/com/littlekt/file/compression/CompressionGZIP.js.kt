f7f7db57af80f46a0a58978928bac5f4edccc394
Colt Daily <LeHaine@users.noreply.github.com>
2024-12-02 15:10:32 -0500

    file: add Compression interface and CompressionGZIP implementations for jvmAndroid & js (#283)

---
+++ core/src/jsMain/kotlin/com/littlekt/file/compression/CompressionGZIP.js.kt
@@ -0,0 +1,70 @@
+package com.littlekt.file.compression
+
+import kotlinx.coroutines.await
+import org.khronos.webgl.Uint8Array
+import org.khronos.webgl.get
+
+/**
+ * JS gzip implementation using the new Streams API
+ *
+ * @author Colton Daily
+ * @date 12/2/2024
+ */
+actual class CompressionGZIP : Compression {
+
+    override suspend fun compress(input: ByteArray): ByteArray {
+        val uint8Array = Uint8Array(input.toTypedArray())
+        val readableStream =
+            ReadableStream(
+                object : UnderlyingSource {
+                    override var start: (controller: ReadableStreamController) -> Unit =
+                        { controller ->
+                            controller.enqueue(uint8Array)
+                            controller.close()
+                        }
+                }
+            )
+        val compressedStream = readableStream.pipeThrough(CompressionStream("gzip"))
+        val reader = compressedStream.getReader()
+        var output = Uint8Array(0)
+        while (true) {
+            val chunk = reader.read().await()
+            if (chunk.done) break
+            val temp = Uint8Array(output.length + chunk.value.length)
+            temp.set(output)
+            temp.set(chunk.value, output.length)
+            output = temp
+        }
+        return ByteArray(output.length) { output[it] }
+    }
+
+    override suspend fun decompress(input: ByteArray): ByteArray {
+        val uint8Array = Uint8Array(input.toTypedArray())
+        val readableStream =
+            ReadableStream(
+                object : UnderlyingSource {
+                    override var start: (controller: ReadableStreamController) -> Unit =
+                        { controller ->
+                            controller.enqueue(uint8Array)
+                            controller.close()
+                        }
+                }
+            )
+        val decompressedStream = readableStream.pipeThrough(DecompressionStream("gzip"))
+        val reader = decompressedStream.getReader()
+        var output = Uint8Array(0)
+        while (true) {
+            val chunk = reader.read().await()
+            if (chunk.done) break
+            val temp = Uint8Array(output.length + chunk.value.length)
+            temp.set(output)
+            temp.set(chunk.value, output.length)
+            output = temp
+        }
+        return ByteArray(output.length) { output[it] }
+    }
+
+    actual companion object {
+        actual operator fun invoke(): CompressionGZIP = CompressionGZIP()
+    }
+}
