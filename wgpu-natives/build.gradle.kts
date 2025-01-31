4835a7f105d00d8a74e338154f9968aab398cfb5
LeHaine <colt.daily@gmail.com>
2025-01-31 16:03:25 -0500

    wgpu-natives: update header & binary download tasks to only run if headers and/or binaries are missing

--- wgpu-natives/build.gradle.kts
+++ wgpu-natives/build.gradle.kts
@@ -8,6 +8,16 @@ plugins {
 
 tasks {
     register("downloadWgpuNativeHeaders", Download::class) {
+        val headersDirectory = file("$projectDir/src/main/c")
+        onlyIf("wgpu headers are empty") {
+            return@onlyIf if (headersDirectory.exists() && headersDirectory.isDirectory) {
+                val headerFiles =
+                    headersDirectory.walkTopDown().filter { it.extension == "h" }.toList()
+                headerFiles.isEmpty()
+            } else {
+                true
+            }
+        }
         val webgpuHeadersSHA: String by rootProject.extra
         val wgpuNativeVersion: String by rootProject.extra
         val webgpuHeaderUrl =
@@ -23,15 +33,32 @@ tasks {
     register("installWgpuNativeHeaders", Copy::class) {
         dependsOn("downloadWgpuNativeHeaders")
         val buildDir = layout.buildDirectory.asFile.get().path
+        val headersDir = "$buildDir/wgpu_headers"
+        val headersDirFile = file(headersDir)
+        onlyIf("WGPU headers are downloaded") {
+            headersDirFile.exists() && headersDirFile.walkTopDown().any { it.extension == "h" }
+        }
 
-        from("$buildDir/wgpu_headers") { include("**.h") }
+        from(headersDir) { include("**.h") }
         into("$projectDir/src/main/c")
+        delete(headersDir)
     }
 
     register("downloadWgpuNativeBinaries", Download::class) {
         val wgpuNativeVersion: String by rootProject.extra
         val baseUrl = "https://github.com/gfx-rs/wgpu-native/releases/download/v$wgpuNativeVersion"
-        onlyIfModified(true)
+        val nativesResourcesDir = file("$projectDir/src/main/resources")
+        val downloadDir =
+            file("${layout.buildDirectory.asFile.get().path}/wgpu_binaries").apply { mkdirs() }
+        onlyIf("binaries are empty") {
+            if (nativesResourcesDir.exists() && nativesResourcesDir.isDirectory) {
+                val nativeFiles =
+                    nativesResourcesDir.walkTopDown().filter { it.extension == "dll" }.toList()
+                nativeFiles.isEmpty()
+            } else {
+                true
+            }
+        }
         src(
             listOf(
                 "$baseUrl/wgpu-windows-x86_64-msvc-release.zip",
@@ -40,22 +67,28 @@ tasks {
                 "$baseUrl/wgpu-linux-x86_64-release.zip",
             )
         )
-        dest(layout.buildDirectory.asFile.get())
+        dest(downloadDir)
     }
 
     register("installWgpuNativeBinaries", Copy::class) {
         dependsOn("downloadWgpuNativeBinaries")
         val buildDir = layout.buildDirectory.asFile.get().path
+        val binariesDir = "$buildDir/wgpu_binaries"
+        val binariesDirFile = file(binariesDir)
+        onlyIf("binaries are downloaded") {
+            binariesDirFile.exists() && binariesDirFile.walkTopDown().any { it.extension == "zip" }
+        }
 
-        from(zipTree("$buildDir/wgpu-windows-x86_64-msvc-release.zip")) { include("**.dll") }
-        from(zipTree("$buildDir/wgpu-macos-x86_64-release.zip")) { include("**.dylib") }
-        from(zipTree("$buildDir/wgpu-macos-aarch64-release.zip")) {
+        from(zipTree("$binariesDir/wgpu-windows-x86_64-msvc-release.zip")) { include("**.dll") }
+        from(zipTree("$binariesDir/wgpu-macos-x86_64-release.zip")) { include("**.dylib") }
+        from(zipTree("$binariesDir/wgpu-macos-aarch64-release.zip")) {
             include("**.dylib")
             rename { it.replace(".dylib", "_aarch64.dylib") }
         }
-        from(zipTree("$buildDir/wgpu-linux-x86_64-release.zip")) { include("**.so") }
+        from(zipTree("$binariesDir/wgpu-linux-x86_64-release.zip")) { include("**.so") }
 
         into("$projectDir/src/main/resources")
+        delete(binariesDir)
     }
 
     register("downloadAndInstallWgpuNativeBinariesAndHeaders") {
