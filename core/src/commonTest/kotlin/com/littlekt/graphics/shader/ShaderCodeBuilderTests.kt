2ce1a3802b083152523a2936e76730108e56f49f
LeHaine <colt.daily@gmail.com>
2025-02-08 18:30:28 -0500

    add more shader struct offset tests

--- core/src/commonTest/kotlin/com/littlekt/graphics/shader/ShaderCodeBuilderTests.kt
+++ core/src/commonTest/kotlin/com/littlekt/graphics/shader/ShaderCodeBuilderTests.kt
@@ -576,7 +576,52 @@ class ShaderCodeBuilderTests {
     }
 
     @Test
-    fun shaderStructsAndOffsets() {
+    fun testSimpleStructOffsets() {
+        val ex1 =
+            shaderStruct("Ex1") {
+                mapOf(
+                    "velocity" to ShaderStructParameterType.WgslType.f32,
+                    "acceleration" to ShaderStructParameterType.WgslType.f32,
+                    "frameCount" to ShaderStructParameterType.WgslType.u32,
+                )
+            }
+        assertEquals(ex1.layout["velocity"]?.offset, 0)
+        assertEquals(ex1.layout["acceleration"]?.offset, 4)
+        assertEquals(ex1.layout["frameCount"]?.offset, 8)
+    }
+
+    @Test
+    fun testStructOffsetsWithVecAndMatrix() {
+        val ex2 =
+            shaderStruct("Ex2") {
+                mapOf(
+                    "scale" to ShaderStructParameterType.WgslType.f32,
+                    "offset" to ShaderStructParameterType.WgslType.vec3f,
+                    "projection" to ShaderStructParameterType.WgslType.mat4x4f,
+                )
+            }
+        assertEquals(ex2.layout["scale"]?.offset, 0)
+        assertEquals(ex2.layout["offset"]?.offset, 16)
+        assertEquals(ex2.layout["projection"]?.offset, 32)
+    }
+
+    @Test
+    fun testStructWithArrayParamsOffsets() {
+        val ex3 =
+            shaderStruct("Ex3") {
+                mapOf(
+                    "transform" to ShaderStructParameterType.WgslType.mat3x3f,
+                    "directions" to
+                        ShaderStructParameterType.Array(ShaderStructParameterType.WgslType.vec3f, 4),
+                )
+            }
+
+        assertEquals(ex3.layout["transform"]?.offset, 0)
+        assertEquals(ex3.layout["directions"]?.offset, 48)
+    }
+
+    @Test
+    fun testStructsOffsetWithStructAndArrayParamsOffsets() {
         val ex4a =
             shaderStruct("Ex4a") { mapOf("velocity" to ShaderStructParameterType.WgslType.vec3f) }
         val ex4 =
