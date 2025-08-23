87af8cc0649ef3339c84186fad1c783565772a59
Omkar Tenkale <39808155+omkar-tenkale@users.noreply.github.com>
2025-08-23 19:55:51 +0530

    Fix comilation issues after adding wasm target support

--- core/src/webMain/kotlin/com/littlekt/audio/WebAudioClip.kt
+++ core/src/webMain/kotlin/com/littlekt/audio/WebAudioClip.kt
@@ -1,28 +1,141 @@
 package com.littlekt.audio
 
-import org.khronos.webgl.ArrayBuffer
+import com.littlekt.util.now
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+import org.w3c.dom.Audio
 
 /**
  * @author Colton Daily
- * @date 11/22/2021
+ * @date 11/23/2021
  */
-external class JsSound
+class WebAudioClip(val assetPath: String) : AudioClip {
 
-external class JsSoundDestination
+    override var volume = 1f
+    override val duration: Duration
+        get() = latestClip.duration.toDouble().seconds
 
-external class JsBufferSource {
-    var buffer: JsSound
+    private val clipPool = mutableListOf(ClipWrapper())
+    private var latestClip = clipPool.first()
+    private var lastPlay = Double.NEGATIVE_INFINITY
 
-    fun connect(destination: JsSoundDestination)
+    private fun nextClip(): ClipWrapper {
+        for (i in clipPool.indices) {
+            if (clipPool[i].clipState == ClipState.STOPPED) {
+                return clipPool[i]
+            }
+        }
+        if (clipPool.size < MAX_CLIP_POOL_SIZE) {
+            val clip = ClipWrapper()
+            clipPool += clip
+            return clip
+        }
+        return clipPool.minByOrNull { it.startTime }!!
+    }
 
-    fun start(delay: Int)
-}
+    override fun play(volume: Float, loop: Boolean) {
+        val t = now()
+        lastPlay = t
+        latestClip =
+            nextClip().apply {
+                this.volume = volume
+                this.loop = loop
+                play()
+            }
+    }
 
-external class AudioContext {
+    override fun stop() {
+        latestClip.stop()
+    }
 
-    val destination: JsSoundDestination
+    override fun resume() {
+        latestClip.resume()
+    }
 
-    fun decodeAudioData(bytes: ArrayBuffer, onLoad: (buffer: JsSound) -> Unit)
+    override fun pause() {
+        latestClip.pause()
+    }
 
-    fun createBufferSource(): JsBufferSource
+    override fun release() {
+        latestClip.stop()
+        clipPool.clear()
+    }
+
+    companion object {
+        const val MAX_CLIP_POOL_SIZE = 5
+    }
+
+    private enum class ClipState {
+        STOPPED,
+        PLAYING
+    }
+
+    private inner class ClipWrapper {
+        val audioElement = Audio(assetPath)
+
+        var volume: Float
+            get() = audioElement.volume.toFloat()
+            set(value) {
+                audioElement.volume = value.toDouble()
+            }
+
+        var currentTime: Float
+            get() = audioElement.currentTime.toFloat()
+            set(value) {
+                audioElement.currentTime = value.toDouble()
+            }
+
+        val duration: Float
+            get() = audioElement.duration.toFloat()
+
+        var isPaused: Boolean = false
+            private set
+
+        var isStarted = false
+
+        var loop: Boolean
+            get() = audioElement.loop
+            set(value) {
+                audioElement.loop = value
+            }
+
+        var clipState = ClipState.STOPPED
+            private set
+
+        var startTime = 0.0
+
+        init {
+            audioElement.onended = {
+                clipState = ClipState.STOPPED
+                true
+            }
+        }
+
+        fun play() {
+            audioElement.pause()
+            currentTime = 0f
+            clipState = ClipState.PLAYING
+            isStarted = true
+            audioElement.play()
+        }
+
+        fun stop() {
+            isPaused = true
+            audioElement.pause()
+            clipState = ClipState.STOPPED
+        }
+
+        fun resume() {
+            audioElement.pause()
+            clipState = ClipState.PLAYING
+            isStarted = true
+            audioElement.play()
+        }
+
+        fun pause() {
+            isPaused = true
+            audioElement.pause()
+            clipState = ClipState.STOPPED
+        }
+    }
 }
